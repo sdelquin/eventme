b1cf3b130cd32846f5dcafe3321fac29cff8326f
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-06-06 20:00:12 +0100

    Refactor to OOP

---
+++ eventme.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+import re
+from sgw.core import SendGrid
+
+
+class EventMe:
+    def __init__(self, base_url, search_url, sg_api, sg_from, sg_from_name):
+        self.base_url = base_url
+        self.search_url = search_url
+        self.email = SendGrid(sg_api, sg_from, sg_from_name)
+
+    def scrap(self, date1, date2):
+        self.title = f"Eventos en Tenerife ({date1} - {date2})"
+        params = {
+            "field_fecha_value[min][date]": date1,
+            "field_fecha_value[max][date]": date2
+        }
+        result = requests.get(url=self.search_url, params=params)
+        # remove invalid tag </br> since it leads wrong beautiful soup
+        content = result.text.replace("</br>", "")
+
+        self.events = []
+        soup = BeautifulSoup(content, "html.parser")
+        for container in soup.find_all("div", "small-post"):
+            img = container.find("div", "thumb").a.img["src"]
+            post = container.find("div", "post-c-wrap")
+            url = self.base_url + post.h4.a["href"]
+            title = re.sub(r"[‘'’]", "", post.h4.a.string).strip()
+            meta = post.find("div", "meta")
+            category, venue = meta.find_all("div", "post-category")
+            if category.a:
+                category = category.a.string.upper()
+            else:
+                category = "SIN CATEGORÍA"
+            venue = venue.a.string
+            date = meta.find("div", "post-date").span.string
+            self.events.append({
+                "img": img,
+                "url": url,
+                "title": title,
+                "category": category,
+                "venue": venue,
+                "date": date
+            })
+
+    def __build_msg(self):
+        msg = ["<table>"]
+        for e in self.events:
+            msg.append(f"""
+                <tr>
+                    <td>
+                        <a href="{e['url']}"><img src="{e['img']}"></a>
+                    </td>
+                    <td>
+                        <b>{e['title']}</b><br>
+                        {e['category']}<br>
+                        {e['url']}<br>
+                        {e['date']}<br>
+                        {e['venue']}
+                    </td>
+                </tr>
+            """)
+        msg.append("</table>")
+        self.msg = "".join(msg)
+
+    def send(self, *args):
+        self.__build_msg()
+        for addr in args:
+            self.email.send(
+                to=addr,
+                subject=self.title,
+                msg=self.msg,
+                html=True
+            )
--- main.py
+++ main.py
@@ -1,57 +1,14 @@
 import config
-import requests
-from bs4 import BeautifulSoup
-import re
-from sgw.core import SendGrid
+from eventme import EventMe
 
 date1 = "07/06/2018"
 date2 = "09/06/2018"
 
-
-params = {
-    "field_fecha_value[min][date]": date1,
-    "field_fecha_value[max][date]": date2
-}
-result = requests.get(url=config.SEARCH_URL, params=params)
-# remove invalid tag </br> since it leads wrong beautiful soup
-content = result.text.replace("</br>", "")
-
-msg = ["<table>"]
-soup = BeautifulSoup(content, "html.parser")
-for container in soup.find_all("div", "small-post"):
-    img = container.find("div", "thumb").a.img["src"]
-    post = container.find("div", "post-c-wrap")
-    url = config.BASE_URL + post.h4.a["href"]
-    title = re.sub(r"[‘'’]", "", post.h4.a.string).strip()
-    meta = post.find("div", "meta")
-    category, venue = meta.find_all("div", "post-category")
-    if category.a:
-        category = category.a.string.upper()
-    else:
-        category = "SIN CATEGORÍA"
-    venue = venue.a.string
-    date = meta.find("div", "post-date").span.string
-
-    msg.append(f"""
-<tr>
-    <td>
-        <a href="{url}"><img src="{img}"></a>
-    </td>
-    <td>
-        <b>{title}</b><br>
-        {category}<br>
-        {url}<br>
-        {date}<br>
-        {venue}
-    </td>
-</tr>
-    """)
-msg.append("</table>")
-
-msg = "".join(msg)
-
-email = SendGrid(
+e = EventMe(
+    config.BASE_URL,
+    config.SEARCH_URL,
     config.SENDGRID["API_KEY"],
     config.SENDGRID["FROM_EMAIL"],
     config.SENDGRID["FROM_NAME"]
 )
+e.scrap(date1, date2)
