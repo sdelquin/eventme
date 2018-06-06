9c4987875ebc4c8e87f4227d54c5343589c5ae03
Sergio Delgado Quintero <sdelquin@gmail.com>
2018-06-06 19:11:47 +0100

    Add sendgrid as mail delivery service

--- config.tmpl.py
+++ config.tmpl.py
@@ -1,2 +1,7 @@
 BASE_URL = "http://lagenda.org"
 SEARCH_URL = BASE_URL + "/programacion/hoy"
+SENDGRID = {
+    "API_KEY": "xxxxxxxx",
+    "FROM_EMAIL": "xxxxxxxx",
+    "FROM_NAME": "xxxxxxxx"
+}
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import config
 import requests
 from bs4 import BeautifulSoup
 import re
+from sgw.core import SendGrid
 
-date1 = "04/06/2018"
-date2 = "10/06/2018"
+date1 = "07/06/2018"
+date2 = "09/06/2018"
+
 
 params = {
     "field_fecha_value[min][date]": date1,
@@ -14,6 +16,7 @@ result = requests.get(url=config.SEARCH_URL, params=params)
 # remove invalid tag </br> since it leads wrong beautiful soup
 content = result.text.replace("</br>", "")
 
+msg = ["<table>"]
 soup = BeautifulSoup(content, "html.parser")
 for container in soup.find_all("div", "small-post"):
     img = container.find("div", "thumb").a.img["src"]
@@ -22,17 +25,33 @@ for container in soup.find_all("div", "small-post"):
     title = re.sub(r"[‘'’]", "", post.h4.a.string).strip()
     meta = post.find("div", "meta")
     category, venue = meta.find_all("div", "post-category")
-    try:
+    if category.a:
         category = category.a.string.upper()
-    except AttributeError:
-        category = "<NO CATEGORIZADO>"
+    else:
+        category = "SIN CATEGORÍA"
     venue = venue.a.string
     date = meta.find("div", "post-date").span.string
 
-    print(f"""
-        {title}
-        {url}
-        {category}
+    msg.append(f"""
+<tr>
+    <td>
+        <a href="{url}"><img src="{img}"></a>
+    </td>
+    <td>
+        <b>{title}</b><br>
+        {category}<br>
+        {url}<br>
+        {date}<br>
         {venue}
-        {date}
+    </td>
+</tr>
     """)
+msg.append("</table>")
+
+msg = "".join(msg)
+
+email = SendGrid(
+    config.SENDGRID["API_KEY"],
+    config.SENDGRID["FROM_EMAIL"],
+    config.SENDGRID["FROM_NAME"]
+)
